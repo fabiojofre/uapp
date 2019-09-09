23844d20ce52d75f9e593f7e32c707ec2057e652
Fabio Freitas <fabiojofre@gmail.com>
2019-09-09 20:50:09 -0300

    Primeiro commit

---
+++ src/main/java/com/jofre/uapp/UappApplication.java
@@ -0,0 +1,13 @@
+package com.jofre.uapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jofre/uapp/UappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jofre.uapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

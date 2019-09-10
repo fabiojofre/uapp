0f0ea39e6f54756f2d113dd22b37b5cd307c061d
Fabio Freitas <fabiojofre@gmail.com>
2019-09-09 21:35:47 -0300

    Rest de teste Areas

--- src/main/java/com/jofre/uapp/resources/AreaResource.java
+++ src/main/java/com/jofre/uapp/resources/AreaResource.java
@@ -1,16 +1,28 @@
 package com.jofre.uapp.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jofre.uapp.domain.Area;
 
 @RestController
 @RequestMapping(value="/areas")
 public class AreaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funciolnando";
+	public List<Area> listar() {
+		Area area1 = new Area(1, "Area 1");
+		Area area2 = new Area(2, "Area 2");
+		
+		List<Area>lista = new ArrayList<>();
+		lista.add(area1);
+		lista.add(area2);
+		
+		return lista;
 	}
 
 }

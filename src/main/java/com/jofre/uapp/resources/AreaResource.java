43a5bc539b4a34e1bcc1dd987a55d5e86a445813
fabiojofre <fabiojofre@gmail.com>
2020-05-15 01:40:27 -0300

    Alteração na AreaServices e AreaResouces adicionando paginações

--- src/main/java/com/jofre/uapp/resources/AreaResource.java
+++ src/main/java/com/jofre/uapp/resources/AreaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -61,5 +63,16 @@ public class AreaResource {
 		List<AreaDTO>listDTO = list.stream().map(obj ->new AreaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 		
+	}
+	//retornar lista por paginação no endpoit
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<AreaDTO>> findPage(
+			@RequestParam(value ="page", defaultValue = "0")Integer page, 
+			@RequestParam(value ="linesPerPage", defaultValue = "24")Integer linesPerPage, 
+			@RequestParam(value ="orderBy", defaultValue = "nome")String orderBy, 
+			@RequestParam(value ="direction", defaultValue = "ASC")String direction){
+		Page<Area> list = service.findPage(page,linesPerPage, orderBy, direction);
+		Page<AreaDTO>listDTO = list.map(obj ->new AreaDTO(obj));
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/com/jofre/uapp/services/AreaService.java
+++ src/main/java/com/jofre/uapp/services/AreaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jofre.uapp.domain.Area;
@@ -42,5 +45,10 @@ public class AreaService {
 	}
 	public List<Area>findAll(){
 		return repo.findAll();
+	}
+	//buscar uma lista por paginação
+	public Page<Area>findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
